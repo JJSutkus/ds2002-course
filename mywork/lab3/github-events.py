e4c538a7b9d17863be979dbe225be607c1fb23d5
JJSutkus <awj8sf@virginia.edu>
2026-02-11 13:43:45 -0500

    python script

--- mywork/lab3/github-events.py
+++ mywork/lab3/github-events.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
-
 import os
 import json
 import requests
 
 GHUSER = os.getenv('GITHUB_USER')
-print(GHUSER)
 
 url = f'https://api.github.com/users/{GHUSER}/events'
-print(url)
+print(f"Fetching GitHub events from: {url}")
 
-r = json.loads(requests.get(url).text)
+response = requests.get(url)
+if response.status_code != 200:
+    raise Exception(f"Failed to fetch data: {response.status_code}")
 
-for x in r[:5]:
-  event = x['type'] + ' :: ' + x['repo']['name']
-  print(event)
+events = json.loads(response.text)
+
+print("\nRecent GitHub activity:\n")
+for x in events[:5]:
+    event = x['type'] + ' :: ' + x['repo']['name']
+    print(event)
