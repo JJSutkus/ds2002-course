c5c346563ae768886a806211cc5b882ce391814e
JonathanJordan_Sutkus <awj8sf@virginia.edu>
2026-02-23 09:50:00 -0500

    sql changes

--- mywork/lab5/initialize.sql
+++ mywork/lab5/initialize.sql
@@ -13,3 +13,27 @@ VALUES("Josh Allen", "Buffalo Bills", 25, 10);
 
 INSERT INTO players (player_name, team, touchdowns, interceptions)
 VALUES("Drake Maye", "New England Patriots", 31, 8);
+
+INSERT INTO players (player_name, team, touchdowns, interceptions)
+VALUES("Sam Darnold", "Seattle Seahawks", 25, 14);
+
+INSERT INTO players (player_name, team, touchdowns, interceptions)
+VALUES("Jared Goff", "Detroit Lions", 34, 8);
+
+INSERT INTO players (player_name, team, touchdowns, interceptions)
+VALUES("Dak Prescott", "Dallas Cowboys", 30, 10);
+
+INSERT INTO players (player_name, team, touchdowns, interceptions)
+VALUES("Trevor Lawrence", "Jacksonville Jaguars", 29, 12);
+
+INSERT INTO players (player_name, team, touchdowns, interceptions)
+VALUES("Bo Nix", "Denver Broncos", 25, 11);
+
+INSERT INTO players (player_name, team, touchdowns, interceptions)
+VALUES("Justin Herbert", "Los Angeles Chargers", 26, 13);
+
+INSERT INTO players (player_name, team, touchdowns, interceptions)
+VALUES("Matthew Stafford", "Los Angeles Rams", 46, 8);
+
+INSERT INTO players (player_name, team, touchdowns, interceptions)
+VALUES("Caleb Williams", "Chicago Bears", 27, 7);
