45c04a47e5babb3a7af66a7d19042cbb666df9be
JonathanJordan_Sutkus <awj8sf@virginia.edu>
2026-04-07 17:58:55 -0400

    Lab 10 progress

---
+++ mywork/lab10/add.sql
@@ -0,0 +1,18 @@
+-- Lab 10 additions --
+INSERT INTO players1 (player_name, team, touchdowns, interceptions)
+VALUES ("Kyler Murray", "Minnesota Vikings", 25, 12);
+
+INSERT INTO players1 (player_name, team, touchdowns, interceptions)
+VALUES ("Jaxson Dart", "New York Giants", 27, 14);
+
+INSERT INTO players1 (player_name, team, touchdowns, interceptions)
+VALUES ("Jayden Daniels", "Washington Commanders", 20, 9);
+
+INSERT INTO teams1 (team_name, team_abr, division, wins)
+VALUES("Minnesota Vikings", "Min", "NFC North", 6);
+
+INSERT INTO teams1 (team_name, team_abr, division, wins)
+VALUES("New York Giants", "NYG", "NFC East", 5);
+
+INSERT INTO teams1 (team_name, team_abr, division, wins)
+VALUES("Washington Commanders", "WAS", "NFC East", 7);
---
+++ mywork/lab10/jokes.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --account=ds2002
+#SBATCH --partition=standard
+#SBATCH --job-name=lolcow_joke
+#SBATCH --output=lolcow_%A_%a.out
+#SBATCH --error=lolcow_%A_%a.err
+#SBATCH --time=00:01:00
+#SBATCH --mem=8G
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --array=1-10
+
+module load apptainer
+apptainer run ~/lolcow-latest.sif
